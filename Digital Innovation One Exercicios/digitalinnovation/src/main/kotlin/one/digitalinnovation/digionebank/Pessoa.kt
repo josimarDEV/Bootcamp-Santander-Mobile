a28b8db3ddfd5cd7ba3eabd0254f53604841c80f
josimarDEV <josimar_504@hotmail.com>
2021-07-03 17:33:23 -0300

    "Aprendizagem do Bootcamp Santander Mobile"

--- Digital Innovation One Exercicios/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Digital Innovation One Exercicios/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,8 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "josimar"
     var cpf:String = "123.123.123.11"
+    private set
 
-  inner class endereco{
-      var rua:String = "Rua Amarildo Filho"
-  }
 }
 
 fun main() {
@@ -14,5 +12,4 @@ fun main() {
 
     println(josimar.nome)
     println(josimar.cpf)
-    println(josimar.endereco().rua)
 }
